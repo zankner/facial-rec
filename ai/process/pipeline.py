ed0cb72a1169a67fd505a6a354c018c1dd220f33
zankner <zackankner@gmail.com>
2019-04-09 13:04:15 -0400

    finished data pipeline

--- ai/process/pipeline.py
+++ ai/process/pipeline.py
@@ -19,40 +19,47 @@ def get_files_and_labels():
         label_path = os.path.join('../data/processed',label)
         if os.path.isdir(label_path):
             images = os.listdir(label_path)
-            image_paths = [os.path.join(label_path, img) for img in images if os.path.isfile(os.path.join(person_path, img))]
+            image_paths = [os.path.join(label_path, img) for img in images if os.path.isfile(os.path.join(label_path, img))]
             for img_path in image_paths:
                 filenames.append(img_path)
-                lables.append(label)
+                labels.append(label)
+    filenames = np.asarray(filenames)
+    labels = np.asarray(labels)
     return filenames, labels 
 
 
-def construct_dataset(filenames, labels, input_dim):
-    dataset = tf.data.Dataset.from_tesnor_slices((filenames, labels))
-    dataset = tf.data.shuffle(len(filenames))
-    dataset = tf.data.map(image_parse, num_parallel_calls=3)
-    dataset = tf.data.map(train_preprocess(input_dim=input_dim), num_parallel_calls=3)
+def construct_dataset(filenames, labels, input_dim,batch_size):
+    dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+    dataset = dataset.shuffle(len(filenames))
+    dataset = dataset.map(lambda x, y:image_parse(x,y,input_dim), num_parallel_calls=3)
+    dataset = dataset.map(lambda x, y:train_preprocess(x,y,batch_size), num_parallel_calls=3)
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(1)
+
+    return dataset
 
 def image_parse(filename, label, input_dim):
     image_string = tf.read_file(filename)
 
-    image = tf.image.decode_(image_string,channels=3)
+    image = tf.image.decode_jpeg(image_string,channels=3)
     
     image = tf.image.convert_image_dtype(image, tf.float32)
 
     image = tf.image.resize_images(image, [input_dim,input_dim])
-    return resized_image, label
+    return image, label
 
-def train_preprocess(image, label):
+def train_preprocess(image, label, batch_size):
     image = tf.image.random_flip_left_right(image)
 
     image = tf.image.random_brightness(image, max_delta=32.0/255.0)
     image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
 
-    image = tf.clib_by_value(image, 0.0, 1.0)
+    image = tf.clip_by_value(image, 0.0, 1.0)
 
     return image, label
 
 
-print(get_files_and_labels()[0])
+data = get_files_and_labels()
+construct_dataset(data[0],data[1],32,32)
+
+
